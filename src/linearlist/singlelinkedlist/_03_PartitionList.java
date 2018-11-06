c5840fc5298e4dd4b899fc53dbdb64c2f7660128
Daffupman <wzjin85@foxmail.com>
2018-11-06 17:55:48 +0800

    standardize the filename

--- src/linearlist/singlelinkedlist/_03_PartitionList.java
+++ src/linearlist/singlelinkedlist/_03_PartitionList.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * @author Daffupman
  * @date 2018/11/5 14:11
  */
-public class PartitionList {
+public class _03_PartitionList {
 
     /**
      * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater
