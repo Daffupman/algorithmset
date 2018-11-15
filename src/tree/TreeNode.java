e8f46f28f676db2ae0fe7fc590a1861f401826aa
Daffupman <wzjin85@foxmail.com>
2018-11-15 16:58:21 +0800

    define the tree node

---
+++ src/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package tree;
+
+/**
+ * Definition for a binary tree node.
+ * @author Daffupman
+ * @date 2018/11/15 16:57
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val){
+        this.val = val;
+    }
+
+}
