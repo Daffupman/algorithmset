ea4320a88e80115e8cefa7402773dd595471c7f6
Daffupman <wzjin85@foxmail.com>
2018-11-18 17:09:24 +0800

    preoder the binary tree

--- src/tree/_01_binarytreeconstruct/_01_BinaryTreePreorderTraversal.java
+++ src/tree/_01_binarytreeconstruct/_01_BinaryTreePreorderTraversal.java
@@ -18,41 +18,26 @@ public class _01_BinaryTreePreorderTraversal {
      * Given a binary tree, return the preorder traversal of its nodes’ values.
      */
 
-    public static void main(String[] args) {
-
-        BinarySearchTree tree = new BinarySearchTree();
-        Integer[] nums = {2,14,53,3,21};
-        for (Integer num : nums) {
-            tree.insert(num);
-        }
-        System.out.println(preorderTraversal(tree.root));
-
-    }
-
+    /**
+     * 先序遍历：root->left->right,root->right->left
+     * 对于首次访问到的节点直接加入list中，
+     * 再使用stack存储当前节点的右子节点（存在的话）
+     */
     public static List<Integer> preorderTraversal(TreeNode root) {
-//        List<Integer> list = new ArrayList<>();
-//        Stack<TreeNode> right = new Stack<>();
-//        TreeNode treeNode = root;
-//        while (treeNode != null){
-//            //当前值加入list
-//            list.add(treeNode.val);
-//            System.out.println(treeNode.val);
-//            //当前节点的右子节点存在，就push进right
-//            if(treeNode.right != null){
-//                right.push(treeNode.right);
-//            }
-//            treeNode = treeNode.left;
-//            if(treeNode == null && !right.isEmpty()){
-//                treeNode = right.pop();
-//            }
-//        }
-//        return list;
         List<Integer> list = new ArrayList<>();
-        TreeNode node = root;
-        while(node != null){
-            list.add(node.val);
-            preorderTraversal(node.left);
-            preorderTraversal(node.right);
+        Stack<TreeNode> right = new Stack<>();
+        TreeNode treeNode = root;
+        while (treeNode != null){
+            //当前值加入list
+            list.add(treeNode.val);
+            //当前节点的右子节点存在，就push进right
+            if(treeNode.right != null){
+                right.push(treeNode.right);
+            }
+            treeNode = treeNode.left;
+            if(treeNode == null && !right.isEmpty()){
+                treeNode = right.pop();
+            }
         }
         return list;
     }
